d619e4dc10feeeb6ef89b78fdc41b3fd9a9fb276
esaruoho <esaruoho@gmail.com>
2025-04-22 00:45:58 +0300

    mac build script tweak

--- rx2/build_mac.sh
+++ rx2/build_mac.sh
@@ -1,2 +1,2 @@
-clang++ rex2decoder_JE.cpp /Users/esaruoho/Downloads/rx2/REX.c -o rex2decoder -I /Users/esaruoho/Downloads/rx2/REXSDK_Mac_1.9.2 -DREX_MAC=1 -DREX_WINDOWS=0 -DREX_DLL_LOADER=1 -framework CoreFoundation
+clang++ rex2decoder_mac.cpp /Users/esaruoho/Downloads/rx2/REX.c -o rex2decoder -I /Users/esaruoho/Downloads/rx2/REXSDK_Mac_1.9.2 -DREX_MAC=1 -DREX_WINDOWS=0 -DREX_DLL_LOADER=1 -framework CoreFoundation
 ./rex2decoder 262.rx2 262.wav 262.txt /Users/esaruoho/Downloads/rx2
